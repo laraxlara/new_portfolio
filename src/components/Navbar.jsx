6d43d5c4e0587fb4353731ea92250a3df7b88af6
lunatechdevlab <lunatechdevlab@gmail.com>
2023-07-14 19:19:17 +0200

    Metadata

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <Link href="#home" class="mr-5 hover:text-[#02dea3]">
+    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <Link href="#home" className="mr-5 hover:text-[#02dea3]">
         Home
       </Link>
-      <Link href="#about" class="mr-5 hover:text-[#02dea3]">
+      <Link href="#about" className="mr-5 hover:text-[#02dea3]">
         About
       </Link>
-      <Link href="#projects" class="mr-5 hover:text-[#02dea3]">
+      <Link href="#projects" className="mr-5 hover:text-[#02dea3]">
         Projects
       </Link>
     </nav>
